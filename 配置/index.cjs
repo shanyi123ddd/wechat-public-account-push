3296d2b87745dbd7f485540512f32a30456baf16
shanyi123ddd <111728036+shanyi123ddd@users.noreply.github.com>
2023-01-20 23:49:55 +0800

    Update and rename config/index.cjs to 配置/index.cjs

---
+++ 配置/index.cjs
@@ -0,0 +1,21 @@
+{
+# 公众号配置
+# 公众号appId
+"app_id": "wxc4f7b56dc2d945f9",
+# 公众号appSecret
+"app_secret": "276815b59ce434699ce1ac2a88893a70",
+# 模板消息id
+"template_id": "RHE12FqJz3r2zSIpB6yFEIiB6gKK7zFJ0_b2ckuKbM4",
+# 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
+"user": ["obhwI6OXhse0Z81KFgtO0YHi_m8o", "obhwI6CmjDq13BasulVs6C8L64U4"],
+
+# 信息配置
+# 所在省份
+"province": "河北",
+# 所在城市
+"city": "保定",
+# 生日1，修改name为对应需要显示的名字，如果生日为农历，在最前面加上r即可
+"birthday": {"name": "宝贝小晨", "birthday": "r2004-01-06"},
+# 在一起的日子，格式同上
+"love_date": "2022-12-25"
+}
